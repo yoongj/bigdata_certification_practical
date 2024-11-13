51d36c93f8601349b5a12949dc23071efb66f131
yoongj <skdbswjd16@gmail.com>
2024-11-13 13:04:49 +0900

    제3유형 1,2번 clear

--- README.md
+++ README.md
@@ -32,5 +32,25 @@
 - 결과 저장
 
 
+### q_3.py
+- 제 3유형
+![image](https://github.com/user-attachments/assets/738523fa-1d86-4985-88e0-d8232d6aeccd)
+- 1) 카이제곱 통계량
+    - scipy.stats.chi2_contingency
+        - **두 개 이상의 범주형 변수**간의 **독립성 검정**
+        - 이차원 배열(행렬)을 입력으로 받음
+        - 통계량, p-value, 자유도, 기대값 테이블 을 반환
+        - 이차원 배열은 각 범주에 해당하는 데이터들의 개수표
+        - pd.crosstab으로 교차표를 만들어 이를 chi2_contingency의 입력으로 사용
 
+    - scipy.stats.chisquare
+        - **한 개의 범주형 변수**분포의 **일치 여부** 검정
+        - 한 변수에 대한 관측값, 기댓값 총 두개의 값을 입력으로 받음
+        - 통계량, p-value 를 반환
+
+- 2) 로지스틱 회귀모형 계수
+    - ~~X['Sex']= X['Sex'].apply(lambda x:1 if x=='male' else 0)~~
+    - X.loc[:,'Sex']= [1 if x=='male' else 0 for x in X['Sex']]
+    - model= LogisticRegression
+    - model.coef_[0] : 독립변수별 계수
 
--- 제2회/q_3.py
+++ 제2회/q_3.py
@@ -1,10 +1,38 @@
 import pandas as pd
+
+# titanic_url = 'https://raw.githubusercontent.com/datasciencedojo/datasets/master/titanic.csv'
+# titanic = pd.read_csv(titanic_url)
+# titanic.to_csv('titanic_url.csv')
+titanic= pd.read_csv('titanic_url.csv')
+
+##### 1) #####
 from scipy.stats import chi2_contingency
+# # print(titanic.columns)
+# ct= pd.crosstab(titanic['Sex'],titanic['Survived'])
+# # print(ct)
 
-titanic= pd.read_csv('titanic_test.csv')
-# print(titanic.columns)
-ct= pd.crosstab(titanic['Sex'],titanic['Survived'])
-print(ct)
+# chi= chi2_contingency(ct)
+# print(f"{chi[0]:.4f}")
+# print(round(chi[0],3))
 
-chi= chi2_contingency()
-print(chi)
+
+##### 2) #####
+# # Gender, SibSp, Parch, Fare를 독립변수, 로지스틱 회귀모형, Parch 계수값
+from sklearn.linear_model import LogisticRegression
+
+X= titanic[['Sex', 'SibSp', 'Parch', 'Fare']]
+y= titanic['Survived']
+# print(X.info())
+
+
+# X['Sex']= [1 if i == 'male' else 0 for i in X['Sex']]
+# X['Sex'] = X['Sex'].apply(lambda x: 1 if x == 'male' else 0)
+X.loc[:,'Sex']= [1 if i == 'male' else 0 for i in X['Sex']]
+print(X)
+
+model= LogisticRegression()
+model.fit(X,y)
+print(round(model.coef_[0][2], 3))
+print(f"{model.coef_[0][2]:.3f}")
+
+
